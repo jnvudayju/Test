4a23153fa6351f591525e37271253eb73513aa37
udayyadav <jnvudayju@gmail.com>
2024-08-30 10:29:33 +0530

    static block concept

--- src/main/java/org/acme/ConstructorChaining.java
+++
@@ -1,6 +0,0 @@
-package org.acme;
-
-public class ConstructorChaining {
-}
-
-
---
+++ src/main/java/org/acme/StaticblockConcept.java
@@ -0,0 +1,13 @@
+package org.acme;
+
+public class StaticblockConcept {
+    static {
+        System.out.println("This is static block");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is main method print");
+    }
+}
+
+
